1c037d944068cf01505024d1b747354850a07669
Mostafa <mostafaghobashytamer@gmail.com>
2025-08-24 11:18:01 +0300

    fix: don't use /api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path("users/", include("users.urls")),
 
     # Courses API
-    path('api/courses/', include('courses.urls')),
+    path('courses/', include('courses.urls')),
 ]
 
 if settings.DEBUG:
