915ec7f6ede43d108e940094a143d82bf9d45f4c
Mostafa <mostafaghobashytamer@gmail.com>
2025-08-26 12:26:25 +0300

    some tweaks before i fix books

--- backend/videos/views.py
+++ backend/videos/views.py
@@ -50,21 +50,21 @@ class RetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
         instance.save()
         new_view = ViewSession(video=instance, student=user)
         new_view.save()
-        
+
         # Get related videos with only title and id
         related_videos = Video.objects.filter(course=instance.course).values('id', 'title')
-        
+
         # Get the main video data
         serializer = self.get_serializer(instance, context={"request": request})
         response_data = serializer.data
-        
+
         # Add related videos to the response
         response_data['related_videos'] = list(related_videos)
 
         # Get related books with only title and id
         books = Book.objects.filter(video=instance).values('id', 'title')
         response_data['books'] = list(books)
-        
+
         return Response(response_data)
 
     def get_queryset(self):
@@ -89,7 +89,7 @@ class LikeView(APIView):
     def post(self, request, pk):
         video = Video.objects.get(id=pk)
         like, created = LikeReaction.objects.get_or_create(student=request.user, video=video)
-        
+
         if created:
             video.likes += 1
             video.save()
